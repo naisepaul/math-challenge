17dcda8563a910f8862941d8c639739759ff998a
naisepaul <>
2023-10-13 11:26:40 +0000

    Updated the game and adjust the spaces

--- game_details.py
+++ game_details.py
@@ -14,32 +14,26 @@ game_details = [
    
     _________________________________________________________________________
    |   ___________________________________________________________________   |
-   |  |                                                                   |  |
    |  |                ===============================                    |  |
    |  |                | |                         | |                    |  |
    |  |                | |   G A M E   R U L E S   | |                    |  |
    |  |                | |                         | |                    |  |
    |  |                ===============================                    |  |
-   |  |                                                                   |  |
    |  |  1 - Its a simple maths challenge game.                           |  |
-   |  |                                                                   |  |
    |  |  2 - Randomly generate 10 math questions, each involving          |  |
    |  |      multiplication, addition, or subtraction, with numbers       |  |
    |  |      ranging between 3 and 15. Here's an example of how to        |  |
    |  |      generate questions:                                          |  |
-   |  |                                                                   |  |
-   |  |         Question 1: Multiply 6 by 9                               |  |
-   |  |         Question 2: Add 11 to 4                                   |  |
-   |  |         Question 3: Subtract 8 from 15                            |  |
+   |  |         Question 1: 6 * 9                                         |  |
+   |  |         Question 2: 11 + 4                                        |  |
+   |  |         Question 3: 8 - 15                                        |  |
    |  |         ...and so on.                                             |  |
-   |  |                                                                   |  |
    |  |  3 - Players must solve each math question as quickly as          |  |
    |  |      possible. There is no specific time limit for individual     |  |
    |  |      questions. Only 3 chance for each question, after that it    |  |                                                                   |  |
    |  |      jump to the next question. Each correct answer has 10 score  |  |
    |  |      and wrong one has -2. Points will calculate by dividing      |  |
    |  |      score by total time.                                         |  |
-   |  |                                                                   |  |
    |  |  4 - The top 15 best points will be saved on a scoreboard.        |  |
    |  |___________________________________________________________________|  |
    |_________________________________________________________________________|
